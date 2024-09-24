5f86575ea533f7069f61440233510113d40f1b7f
Joker3052 <115540375+Joker3052@users.noreply.github.com>
2024-09-24 15:34:32 +0700

    Update axios-customize.js

--- fe_tttn-main/src/Services/axios-customize.js
+++ fe_tttn-main/src/Services/axios-customize.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const instance = axios.create({
-    baseURL: 'http://localhost:8080' 
-    // baseURL: 'https://backend-tttn.vercel.app'  
+    baseURL: 
+
   });
   // Add a response interceptor
   instance.interceptors.response.use(function (response) {
